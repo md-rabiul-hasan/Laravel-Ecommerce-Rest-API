a56e118806b5790a4edc683dcc3e8e7134a29d20
mdrabiulhasan <mdrabiulhasan.me@gmail.com>
2022-04-23 01:53:46 +0600

    factory and seeder

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'product_id',
+        'customer',
+        'review',
+        'star'
+    ];
+
 }
--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,10 +2,18 @@
 
 namespace Database\Factories;
 
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ProductFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
     /**
      * Define the model's default state.
      *
@@ -14,7 +22,11 @@ class ProductFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name'     => $this->faker->word,
+            'details'  => $this->faker->paragraph,
+            'price'    => $this->faker->numberBetween(100,500),
+            'stock'    => $this->faker->randomDigit,
+            'discount' => $this->faker->numberBetween(2,10),
         ];
     }
 }
