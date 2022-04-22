956b6366b03b9f612ca497ddd07f178a12f3cef9
mdrabiulhasan <mdrabiulhasan.me@gmail.com>
2022-04-23 02:33:51 +0600

    transform products

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -15,11 +15,16 @@ class ProductResource extends JsonResource
     public function toArray($request)
     {
         return [
-            "name"        => $this->name,
-            "description" => $this->details,
-            "price"       => number_format($this->price,2),
-            "stock"       => $this->stock,
-            "discount"    => $this->discount." %"
+            "name"          => $this->name,
+            "description"   => $this->details,
+            "price"         => number_format($this->price,2),
+            "stock"         => $this->stock > 0 ? $this->stock : 'Out of Stock',
+            "discount"      => $this->discount." %",
+            "current_price" => ($this->price - (($this->price / 100) * $this->discount)),
+            "rating"        => ($this->reviews->sum('star') / $this->reviews->count()),
+            "reviews"       => [
+                "href" => route('reviews.index', $this->id)
+            ]
         ];
     }
 }
